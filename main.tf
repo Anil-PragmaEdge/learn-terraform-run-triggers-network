a8b41e68e9cc45550307b8cb366179e443f36641
Anil-PragmaEdge <93245991+Anil-PragmaEdge@users.noreply.github.com>
2021-11-10 21:25:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,10 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region  = var.aws_region
-  profile = "bojja"
+  region                = var.aws_region
+  profile               = "bojja"
+  aws_access_key_id     = var.aws_access_key
+  aws_secret_access_key = var.aws_secret_access_key
 }
 
 data "aws_availability_zones" "available" {
